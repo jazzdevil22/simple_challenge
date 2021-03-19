dadd9dac5c9336d4bf7e808ea58a02e353830a8c
jazzdevil22 <63672958+jazzdevil22@users.noreply.github.com>
2021-03-19 21:02:52 +0000

    Data verification complete for now

--- app/simple_challenge_app.py
+++ app/simple_challenge_app.py
@@ -1,5 +1,49 @@
+def verify_3D_coordinate_data(data_set):
+	for coordinate in data_set:
+		if len(coordinate) != 3:
+			return False
+
+		for point in coordinate:
+			if not (type(point) == float):
+				return False
+
+	return True
+
+def verify_quadrilateral_data(data_set, coordinate_count):
+	for quadrilateral in data_set:
+		if len(quadrilateral) != 4:
+			return False
+
+		for point in quadrilateral:
+			if point >= coordinate_count or point < 0:
+				return False
+
+			if not isinstance(point, int):
+				return False
+
+	return True
+
 def verify_data(data):
-	if len(data.keys()) != 2:
+	coordinate_length = 3
+	coordinate_key = "p"
+	quadrilateral_data_length = 4
+	quadrilateral_key = "q"
+	no_of_data_sets = 2
+
+	if len(data.keys()) != no_of_data_sets:
 		return False
-	else :
+
+	if coordinate_key not in data.keys():
+		return False
+
+	if quadrilateral_key not in data.keys():
+		return False
+
+	if verify_3D_coordinate_data(data.get(coordinate_key)) == False:
+		return False
+
+	if verify_quadrilateral_data(data.get(quadrilateral_key), len(data.get(coordinate_key))) == False:
+		return False
+
+	else:
 		return True
--- tests/test_simple_challenge_app.py
+++ tests/test_simple_challenge_app.py
@@ -5,7 +5,7 @@ from .app_context import app
 
 def test_verifying_data_from_json_file_with_correct_format_returns_True():
 	#given
-	test_json_string = '''{"q": [[23, 22, 0, 1]], "p": [[2.9, 0.0, -0.5], [2.88, 0.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
+	test_json_string = '''{"q": [[1, 2, 3, 0]], "p": [[2.9, 0.0, -0.5], [2.88, 0.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
 	test_data = json.loads(test_json_string)
 
 	#when
@@ -16,7 +16,7 @@ def test_verifying_data_from_json_file_with_correct_format_returns_True():
 
 def test_verifying_data_from_json_file_with_too_many_keys_returns_False():
 	#given
-	test_json_string = '''{"r": [[1, 2]], "q": [[23, 22, 0, 1]], "p": [[2.9, 0.0, -0.5], [2.88, 0.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
+	test_json_string = '''{"r": [[1, 2]], "q": [[1, 2, 3, 0]], "p": [[2.9, 0.0, -0.5], [2.88, 0.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
 	test_data = json.loads(test_json_string)
 
 	#when
@@ -28,6 +28,83 @@ def test_verifying_data_from_json_file_with_too_many_keys_returns_False():
 def test_verifying_data_from_json_file_with_too_few_keys_returns_False():
 	#given
 	test_json_string = '''{"p": [[2.9, 0.0, -0.5], [2.88, 0.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
+	test_data = json.loads(test_json_string)
+
+	#when
+	result = app.verify_data(test_data)#function under test
+	expected = False
+
+	assert result == expected
+
+def test_verifying_data_from_json_file_with_incorrect_keys_returns_False():
+	#given
+	test_json_string = '''{"r": [[1, 2, 3, 0]], "p": [[2.9, 0.0, -0.5], [2.88, 0.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
+	test_data = json.loads(test_json_string)
+
+	#when
+	result = app.verify_data(test_data)#function under test
+	expected = False
+
+	assert result == expected
+
+def test_verifying_data_from_json_file_with_incorrect_coordinate_format_returns_false():
+	#given
+	test_json_string = '''{"q": [[1, 2, 3, 0]], "p": [[2.9, 6.8, 0.0], [2.88, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
+	test_data = json.loads(test_json_string)
+
+	#when
+	result = app.verify_data(test_data)#function under test
+	expected = False
+
+	assert result == expected
+
+def test_verifying_data_from_json_file_with_incorrect_quadrilateral_format_returns_false():
+	#given
+	test_json_string = '''{"q": [[1, 2, 3, 0, 1]], "p": [[2.9, 6.8, 0.0], [2.88, 9.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
+	test_data = json.loads(test_json_string)
+
+	#when
+	result = app.verify_data(test_data)#function under test
+	expected = False
+
+	assert result == expected
+
+def test_verifying_data_from_json_file_returns_false_if_a_quadrilateral_point_exceeds_the_number_of_coordinates():
+	#given
+	test_json_string = '''{"q": [[23, 22, 0, 1]], "p": [[2.9, 0.0, -0.5], [2.88, 0.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
+	test_data = json.loads(test_json_string)
+
+	#when
+	result = app.verify_data(test_data)#function under test
+	expected = False
+
+	assert result == expected
+
+def test_verifying_data_from_json_file_returns_false_if_a_quadrilateral_point_is_negative():
+	#given
+	test_json_string = '''{"q": [[3, 2, 0, -1]], "p": [[2.9, 0.0, -0.5], [2.88, 0.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
+	test_data = json.loads(test_json_string)
+
+	#when
+	result = app.verify_data(test_data)#function under test
+	expected = False
+
+	assert result == expected
+
+def test_verifying_data_from_json_file_returns_false_if_a_coordinate_is_not_a_float():
+	#given
+	test_json_string = '''{"q": [[3, 2, 0, 1]], "p": [[2.9, 0.0, -0.5], [2, 0, 6], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
+	test_data = json.loads(test_json_string)
+
+	#when
+	result = app.verify_data(test_data)#function under test
+	expected = False
+
+	assert result == expected
+
+def test_verifying_data_from_json_file_returns_false_if_a_quadrilateral_point_is_not_an_int():
+	#given
+	test_json_string = '''{"q": [[3, 2, 0.0, 1]], "p": [[2.9, 0.0, -0.5], [2.88, 0.0, -0.8], [2.82, 0.0, -1.0], [2.75, 0.0, -1.65]]}'''
 	test_data = json.loads(test_json_string)
 
 	#when
